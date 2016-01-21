25cd6b5bd3ef075ec975fbd4f6826c78d5853023
n0va <hilmar.wiegand@flameshome.net>
2016-01-21 13:36:13 +0100

    image class cleaned up.

--- include/image.h
+++ include/image.h
@@ -50,60 +50,63 @@ struct RGBA {
 
 class image {
 	public:
-	image(const string& name);
+        image(const string& name);
+        ~image();
 
-	vector<vector<RGBA>> pixels() { return pixels_; }
+        // Getter
+        vector<vector<RGBA>> pixels() { return this->pixels_; }
+        const string& name() const { return this->name_; }
+        const Size& size() const { return this->size_; }
+        const Size& o_size() const { return this->o_size_; }
+        const Rect& bounds() const { return this->bounds_; }
+        const Pos& position() const { return this->position_; }
+        const Pos& offset() const { return this->offset_; }
 
-	const string& name() const { return name_; }
-	const Size& size() const { return size_; }
-	const Size& o_size() const { return o_size_; }
-	const Rect& bounds() const { return bounds_; }
-	const Pos& position() const { return position_; }
-	void position(const Pos& position) { position_ = position; }
-	const Pos& offset() const { return offset_; }
+        // Setter
+        void position(const Pos& position) { this->position_ = position; }
 
-	// Raw pixel input/output
-	void set_pixels(vector<unsigned char>& raw, 
-			unsigned int raw_width,
-			unsigned int raw_height);
+        // Raw pixel input/output
+        void set_pixels(vector<unsigned char>& raw, 
+                        unsigned int raw_width,
+                        unsigned int raw_height);
 
-	vector<unsigned char> get_pixels(); 
+        vector<unsigned char> get_pixels(); 
 
-	void set_size(Size size);
+        void set_size(Size size);
 
-	// comparing images
-	static bool compare_area(const image& img1, const image& img2) {
-		return (img1.get_area() > img2.get_area());	
-	}
+        // Image comparison
+        static inline bool compare_area(const image& img1, const image& img2) {
+            return (img1.get_area() > img2.get_area());	
+        }
 
-	inline int get_area() const {
-		return (size_.width * size_.height);
-	}
+        inline int get_area() const {
+            return (this->size_.width * this->size_.height);
+        }
 
-	// Image manipulation
-	void append_width(unsigned int new_width);
-	void append_height(unsigned int new_height);
+        // Image manipulation
+        void append_width(unsigned int new_width);
+        void append_height(unsigned int new_height);
 
-	void append_image_bot(image& image_to_append);
-	void append_image_right(image& image_to_append);
+        void append_image_bot(image& image_to_append);
+        void append_image_right(image& image_to_append);
 
-	void insert_image(Pos& pos, image& image_to_insert);
+        void insert_image(Pos& pos, image& image_to_insert);
 
-	void trim();
-	void draw_bounds();
+        void trim();
+        void draw_bounds();
 
-	// reading/writing 
-	void read_file(const string& filename);
-	void write_file(const string& filename);
+        // File I/O 
+        void read_file(const string& filename);
+        void write_file();
 
 	private:
-	string name_;
-	vector<vector<RGBA>> pixels_;
-	Size size_;
-	Size o_size_;
-	Rect bounds_;
-	Pos position_;
-	Pos offset_;
+        string name_;
+        vector<vector<RGBA>> pixels_;
+        Size size_;
+        Size o_size_;
+        Rect bounds_;
+        Pos position_;
+        Pos offset_;
 };
 
 #endif
--- src/image.cpp
+++ src/image.cpp
@@ -1,186 +1,188 @@
-#include "image.h"
+#include <image.h>
 
-#include "../../../lib/lodepng/lodepng.h"
+#include <lodepng/lodepng.h>
 
 using namespace std;
 
-image::image(const string& name) : name_(name), 
+image::image(const string& name) : 
+                   name_(name), 
 		   		   size_(Size()), 
 		   		   o_size_(Size()), 
 		   		   bounds_(Rect()),
 				   offset_(Pos(0, 0)) {}
 
+image::~image() {
+}
 
 void image::set_pixels(vector<unsigned char>& raw, 
 		       unsigned int raw_width,
 		       unsigned int raw_height) {
-	size_.width = raw_width;
-	size_.height = raw_height;
+	this->size_.width = raw_width;
+	this->size_.height = raw_height;
 
-	o_size_.width = raw_width;
-	o_size_.height = raw_height;
+	this->o_size_.width = raw_width;
+	this->o_size_.height = raw_height;
 
-	for (unsigned int y = 0; y < size_.height; ++y) {
+	for (unsigned int y = 0; y < this->size_.height; ++y) {
 		vector<RGBA> row;
-		unsigned int row_start = y * 4 * size_.width;
+		unsigned int row_start = y * 4 * this->size_.width;
 
-		for (unsigned int x = 0; x < size_.width; ++x) {
+		for (unsigned int x = 0; x < this->size_.width; ++x) {
 			RGBA new_pixel(raw[row_start + (x * 4)],     // R
 				       raw[row_start + (x * 4) + 1], // G
 				       raw[row_start + (x * 4) + 2], // B
 				       raw[row_start + (x * 4) + 3]);// A
 			row.push_back(new_pixel);
 		}
-		pixels_.push_back(row);
+		this->pixels_.push_back(row);
 	}
 }
 
 vector<unsigned char> image::get_pixels() {
 	vector<unsigned char> raw;
-	for (vector<RGBA> current_row : pixels_)
+	for (vector<RGBA> current_row : this->pixels_)
 		for (RGBA current_pixel : current_row) {
 			raw.push_back(current_pixel.R);
 			raw.push_back(current_pixel.G);
 			raw.push_back(current_pixel.B);
 			raw.push_back(current_pixel.A);
 		}
-
 	return raw;
 }
 
 void image::set_size(Size size) {
-	size_ = size;
-	pixels_.resize(size_.height);
-	for (vector<RGBA>& current_row : pixels_)
-		current_row.resize(size_.width, RGBA());
+	this->size_ = size;
+	this->pixels_.resize(this->size_.height);
+	for (vector<RGBA>& current_row : this->pixels_)
+		current_row.resize(this->size_.width, RGBA());
 }
 
 
 void image::append_width(unsigned int new_width) {
-	if (new_width <= size_.width) 
-		cout << "append width failed: new width too small" << endl;
-	else {
-		unsigned int w_diff = new_width - size_.width;
+	if (new_width <= this->size_.width) {
+		cout << "Append width failed: new width too small" << endl;
+        return;
+    }
 
-		for (vector<RGBA>& current_row : pixels_)
-			for (unsigned int i = 0; i < w_diff; ++i)
-				current_row.push_back(RGBA());
-		
-		size_.width = new_width;
-	}
+    unsigned int w_diff = new_width - this->size_.width;
+
+    for (vector<RGBA>& current_row : this->pixels_)
+        for (unsigned int i = 0; i < w_diff; ++i)
+            current_row.push_back(RGBA());
+    
+    this->size_.width = new_width;
 }
 
 void image::append_height(unsigned int new_height) {
-	if (new_height <= size_.height) 
-		cout << "append height failed: new height too small" << endl;
-	else {
-		unsigned int h_diff = new_height - size_.height;
+	if (new_height <= this->size_.height) {
+		cout << "Append height failed: new height too small" << endl;
+        return;
+    }
 
-		for (unsigned int y = 0; y < h_diff; ++y) {
-			vector<RGBA> new_row;
+    unsigned int h_diff = new_height - this->size_.height;
 
-			for (unsigned int x = 0; x < size_.width; ++x)
-				new_row.push_back(RGBA());
-			
-			pixels_.push_back(new_row);
-		}
+    for (unsigned int y = 0; y < h_diff; ++y) {
+        vector<RGBA> new_row;
 
-		size_.height = new_height;
-	}
+        for (unsigned int x = 0; x < this->size_.width; ++x)
+            new_row.push_back(RGBA());
+        
+        this->pixels_.push_back(new_row);
+    }
+
+    this->size_.height = new_height;
 }
 
 void image::append_image_bot(image& image_to_append) {
 	
-	// bring both images to the same width
-	if (size_.width < image_to_append.size().width)
+	// Bring both images to the same width
+	if (this->size_.width < image_to_append.size().width)
 		append_width(image_to_append.size().width);
-	else if (size_.width > image_to_append.size().width)
-		image_to_append.append_width(size_.width);
+	else if (this->size_.width > image_to_append.size().width)
+		image_to_append.append_width(this->size_.width);
 
-	// append new rows from other image at bottom
+	// Append new rows from other image at bottom
 	for (vector<RGBA> row : image_to_append.pixels())
-		pixels_.push_back(row);
+		this->pixels_.push_back(row);
 
-	// update height
-	size_.height += image_to_append.size().height;
+	// Update height
+	this->size_.height += image_to_append.size().height;
 }
 
 void image::append_image_right(image& image_to_append) {
 
-	// bring both images to the same height
-	if (size_.height < image_to_append.size().height)
+	// Bring both images to the same height
+	if (this->size_.height < image_to_append.size().height)
 		append_height(image_to_append.size().height);
-	else if (size_.height > image_to_append.size().height)
-		image_to_append.append_height(size_.height);
+	else if (this->size_.height > image_to_append.size().height)
+		image_to_append.append_height(this->size_.height);
 
-	// append new rows from other image to the right of each row
-	for (int y = 0; 
-	     y < (int) image_to_append.size().height; 
-	     ++y) 
-		for (int x = 0;
-		     x < (int) image_to_append.size().width;
-		     ++x) 
-		     	pixels_.at(y).push_back(image_to_append.pixels().at(y).at(x));
+	// Append new rows from other image to the right of each row
+	for (int y = 0; y < (int) image_to_append.size().height; ++y) 
+		for (int x = 0; x < (int) image_to_append.size().width; ++x) 
+		     	this->pixels_.at(y).push_back(image_to_append.pixels().at(y).at(x));
 
 
-	size_.width += image_to_append.size().width;
+	this->size_.width += image_to_append.size().width;
 
+#if DEBUG
 	cout << "images merged. array sizes: "
-	     << pixels_.at(1).size()
+	     << this->pixels_.at(1).size()
 	     << " width, "
-	     << pixels_.size()
+	     << this->pixels_.size()
 	     << " height. image size: "
-	     << size_.width
+	     << this->size_.width
 	     << ", "
-	     << size_.height
+	     << this->size_.height
 	     << endl;
-	// update width
-
+#endif
 }
 
 void image::insert_image(Pos& pos, image& image_to_insert) {
-	// check if it fits
-	if (pos.x + image_to_insert.size().width > size_.width ||
-	    pos.y + image_to_insert.size().height > size_.height) {
-		cout << "image doesnt fit!" << endl;
+	// Check dimensions
+	if (pos.x + image_to_insert.size().width > this->size_.width ||
+	    pos.y + image_to_insert.size().height > this->size_.height) {
+		cout << "Cant insert image: Size mismatch!" << endl;
 		return;
 	}
+
 	for (int y = 0; y < image_to_insert.size().height; ++y)
 		for (int x = 0; x < image_to_insert.size().width; ++x) 
-			pixels_[y + pos.y][x + pos.x] = image_to_insert.pixels().at(y).at(x);
+			this->pixels_[y + pos.y][x + pos.x] = image_to_insert.pixels().at(y).at(x);
 		
 }
 
 void image::trim() {
-	// Trim rows from top down
 	bool is_empty = true;
 	unsigned int i = 0;
+
+	// Trim rows from top down
 	do {
-		for (RGBA& current_pixel : pixels_.front()) 
+		for (RGBA& current_pixel : this->pixels_.front()) 
 			if (current_pixel.A != 0) {
 				is_empty = false;
 				break;
 			}
 		if (is_empty) {
-			pixels_.erase(pixels_.begin());
-			size_.height -= 1;
-			offset_.y += 1;
+			this->pixels_.erase(pixels_.begin());
+			this->size_.height -= 1;
+			this->offset_.y += 1;
 			++i;
 		}
-	} while (is_empty && i < size_.height);
+	} while (is_empty && i < this->size_.height);
 	
 	// Trim rows from bottom up
 	is_empty = true;
-	i = size_.height;
+	i = this->size_.height;
 	do {
-		for (RGBA& current_pixel : pixels_.back()) 
+		for (RGBA& current_pixel : this->pixels_.back()) 
 			if (current_pixel.A != 0) {
 				is_empty = false;
 				break;
 			}
 		if (is_empty) {
-			pixels_.pop_back();
-			size_.height -= 1;
+			this->pixels_.pop_back();
+			this->size_.height -= 1;
 			--i;
 		}
 	} while (is_empty && i > 0);
@@ -189,60 +191,61 @@ void image::trim() {
 	is_empty = true;
 	i = 0;
 	do {
-		for (vector<RGBA>& current_row : pixels_) 
+		for (vector<RGBA>& current_row : this->pixels_) 
 			if (current_row.front().A != 0) {
 				is_empty = false;
 				break;
 			}
 		if (is_empty) {
-			for (vector<RGBA>& current_row : pixels_)
+			for (vector<RGBA>& current_row : this->pixels_)
 				current_row.erase(current_row.begin());
-			size_.width -= 1;
-			offset_.x += 1;	
+			this->size_.width -= 1;
+			this->offset_.x += 1;	
 			++i;
 		}	
-	} while (is_empty && i < size_.width);
+	} while (is_empty && i < this->size_.width);
 
 	// Trim from right to left
 	is_empty = true;
-	i = size_.width;
+	i = this->size_.width;
 	do {
 
-		for (vector<RGBA>& current_row : pixels_) 
+		for (vector<RGBA>& current_row : this->pixels_) 
 			if (current_row.back().A != 0) {
 				is_empty = false;
 				break;
 			}
 		if (is_empty) {
-			for (vector<RGBA>& current_row : pixels_)
+			for (vector<RGBA>& current_row : this->pixels_)
 				current_row.pop_back();
-			size_.width -= 1;
+			this->size_.width -= 1;
 			--i;
 		}
 	} while (is_empty && i > 0);
 }
 
 void image::draw_bounds() {
-	// first and last row
-	for (RGBA& current_pixel : pixels_.front()) {
+	// First and last row
+	for (RGBA& current_pixel : this->pixels_.front()) {
 		current_pixel.R = 0;
 		current_pixel.G = 0;
 		current_pixel.B = 0;
 		current_pixel.A = 255;
 	}
-	for (RGBA& current_pixel : pixels_.back()) {
+	for (RGBA& current_pixel : this->pixels_.back()) {
 		current_pixel.R = 0;
 		current_pixel.G = 0;
 		current_pixel.B = 0;
 		current_pixel.A = 255;
 	}
 
-	// sides
-	for (vector<RGBA>& current_row : pixels_) {
+	// Sides
+	for (vector<RGBA>& current_row : this->pixels_) {
 		current_row.front().R = 0;		
 		current_row.front().G = 0;		
 		current_row.front().B = 0;		
 		current_row.front().A = 255;
+
 		current_row.back().R = 0;		
 		current_row.back().G = 0;		
 		current_row.back().B = 0;		
@@ -259,28 +262,31 @@ void image::read_file(const string& filename) {
 					 raw_height,
 					 filename);
 
-	if (error) cout << "error reading file: " 
-			<< lodepng_error_text(error)
-			<< endl;
+	if (error) cout << "Error reading file: " 
+			        << lodepng_error_text(error)
+			        << endl;
 	else set_pixels(raw, raw_width, raw_height);
 }
 
-void image::write_file(const string& filename) {
+void image::write_file() {
 	// encode image and write to file
-	unsigned error = lodepng::encode(filename,
+	unsigned error = lodepng::encode(name_,
 					 get_pixels(),
 					 size_.width,
 					 size_.height);
-	cout << "trying to write " << filename 
-		<< " with " 
-		<< size_.width << " width and "
-		<< size_.height << " height."
-		<< endl;
+	cout << "Writing file \"" 
+         << this->name_ 
+		 << "\" with " 
+		 << this->size_.width << " width and "
+		 << this->size_.height << " height."
+		 << endl;
 
-	if (error) cout << "error writing file: " 
-			<< lodepng_error_text(error) 
-			<< endl;
-	else cout << filename << " written." << endl;
-	
+	if (error) cout << "Error writing file: " 
+			        << lodepng_error_text(error) 
+			        << endl;
+	else cout << "\""
+              << this->name_
+              << "\" written."
+              << endl;	
 }
 
